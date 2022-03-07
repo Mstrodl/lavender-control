ec1a1a967b09d5541c4ecbbfff87cde9103b7d7e
Mary Strodl <ipadlover8322@gmail.com>
2022-03-07 12:45:35 -0500

    configure_r10k: install deps

--- configure_r10k.sh
+++ configure_r10k.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+puppet module install puppetlabs-inifile --version 5.2.0 --ignore-dependencies
+puppet module install puppetlabs-vcsrepo --version 5.0.0 --ignore-dependencies
+puppet module install choria-mcollective --version 0.14.1 --ignore-dependencies
+puppet module install puppet-systemd --version 3.8.0 --ignore-dependencies
+puppet module install puppet-r10k --version 10.1.1 --ignore-dependencies
+
 puppet apply configure_r10k.pp
