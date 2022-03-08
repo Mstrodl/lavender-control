9b9a777e0e95e99233a6df3b8dee6641c399c624
Mary Strodl <ipadlover8322@gmail.com>
2022-03-07 21:42:44 -0500

    development_tools: remove .emacs.d

--- site/profile/manifests/development_tools.pp
+++ site/profile/manifests/development_tools.pp
@@ -2,16 +2,6 @@ class profile::development_tools(
   String $wakatime_config,
   String $eyaml_public_key,
 ) {
-  vcsrepo { '/home/mary/.emacs.d':
-    ensure => latest,
-    provider => git,
-    remote => 'origin_https',
-    revision => 'master',
-    source => {
-      'origin_https' => 'https://github.com/Mstrodl/.emacs.d.git',
-      'origin' => 'git@github.com/Mstrodl/.emacs.d.git',
-    },
-  }
   package { 'emacs':
     ensure => latest,
   }
@@ -45,8 +35,15 @@ class profile::development_tools(
 
   file { '/home/mary/.eyaml':
     ensure => directory,
+    owner => 'mary',
   } -> file { '/home/mary/.eyaml/config.yaml':
     ensure => present,
     content => $eyaml_public_key,
+    owner => 'mary',
+  }
+
+  file { '/home/mary/projects':
+    ensure => directory,
+    owner => 'mary',
   }
 }
