35953059963db801fbbd4c53b183d34e81f32a52
Mary Strodl <ipadlover8322@gmail.com>
2022-03-07 21:51:30 -0500

    ssh: add keys to all users

--- site/profile/manifests/ssh.pp
+++ site/profile/manifests/ssh.pp
@@ -1,3 +1,17 @@
-class profile::ssh {
-  include ::ssh
+class profile::ssh(
+  Hash[String, Hash[String, String]] $keys,
+) {
+  $keys_new = {}
+  $keys.each |$name, $key| {
+    ['root', 'mary'].each |$username| {
+      if User[$username] {
+        $keys_new[$username + "_" + $name] = $key + {
+          user => $username,
+        }
+      }
+    }
+  }
+  class { 'ssh':
+    keys => $keys_new,
+  }
 }
